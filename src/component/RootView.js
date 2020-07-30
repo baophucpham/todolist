8738a827f1ec1398a3604440c37b09d2886ce1f8
baophucpham <phucpham@meu-solutions.com>
2020-07-30 14:09:04 +0700

    todolist-fale

--- src/component/RootView.js
+++ src/component/RootView.js
@@ -1,11 +1,20 @@
-import {View,Text} from 'react-native';
+import { View, Text } from 'react-native';
 import *as React from 'react';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default class RootView extends React.Component{
-    render(){
-        return(
-            <View>
-                <Text>RoodView</Text>
+import { Home } from "./Atom/Home";
+import { List } from "./Atom/List";
+
+const Tab = createBottomTabNavigator();
+
+export default class RootView extends React.Component {
+    render() {
+        return (
+           <View>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={Home} />
+                <Tab.Screen name="List" component={List} />
+            </Tab.Navigator>
             </View>
         )
     }
